1a681509b633ce96e1c8a66aa1b3eee1793b5a98
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-13 17:30:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  attr_accessor :brand, :size, :material,
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def condition= (condition="tattered")
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+    
+ 
+  def cooble ()
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
